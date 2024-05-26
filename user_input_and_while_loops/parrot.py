63af6b511467361a00cbef84551915268af19237
imdylanpro <159092190+imdylanpro@users.noreply.github.com>
2024-05-26 12:17:37 -0700

    Create parrot.py

---
+++ user_input_and_while_loops/parrot.py
@@ -0,0 +1,13 @@
+# Dylan Nelson
+# February 21, 2024
+# parrot.py
+
+# This program will get a user's input and speak it back to them
+
+message = input("Tell me something, and I will repeat it back to you: ")
+
+print(message)
+
+name = input("What is your name?\n")
+
+print(f"Your name is {name.title()}.")
