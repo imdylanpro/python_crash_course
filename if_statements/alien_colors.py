931658478b7ebf9afcb02ea7c772794d1a65f654
imdylanpro <159092190+imdylanpro@users.noreply.github.com>
2024-05-26 12:13:39 -0700

    Create alien_colors.py

---
+++ if_statements/alien_colors.py
@@ -0,0 +1,18 @@
+# Dylan Nelson
+# February 09, 2024
+# alien_colors.py
+
+# This program will check what color an alien variable is and how many points
+# it is worth, then it will send a message to the player telling them the info,
+
+alien_color = 'blue'
+
+if alien_color == 'green':
+    print("The color is green.")
+    print("You have gained 1 point.")
+elif alien_color == 'red':
+    print("The color is red.")
+    print("You have gained 2 points.")
+else:
+    print("The color is blue.")
+    print("You have gained 3 points.")
