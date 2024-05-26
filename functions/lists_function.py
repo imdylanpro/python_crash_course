9466191610d43121209ecf6ea4187378df1755a6
imdylanpro <159092190+imdylanpro@users.noreply.github.com>
2024-05-26 12:09:06 -0700

    Create lists_function.py

---
+++ functions/lists_function.py
@@ -0,0 +1,11 @@
+# Dylan Nelson
+# May 02, 2024
+# lists_function.py
+
+def greet_users(names):
+    '''This function will print all of the users that are within users list'''
+    for name in names:
+        print(name)
+
+names = ['Mary', 'Thomas', 'Jerry', 'Michael']
+greet_users(names)
