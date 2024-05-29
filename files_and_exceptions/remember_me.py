38abd25946f6121da809477265a53d46f3ee2458
Dylan Nelson <159092190+imdylanpro@users.noreply.github.com>
2024-05-29 16:54:53 -0700

    Update remember_me.py
    Added the functionality to include the city and age of the user on top of the name. Renamed old variables to be more fitting with the new changes.

--- files_and_exceptions/remember_me.py
+++ files_and_exceptions/remember_me.py
@@ -5,40 +5,57 @@
 from pathlib import Path
 import json
 
-path = Path('json_files/username.json')
+path = Path('json_files/name.json')
 
-def get_stored_username(path):
-    '''Get stored username if available.'''
-    if path.exists():
-        contents = path.read_text(encoding='utf-8')
-        username = json.loads(contents)
-        return username
-    else:
-        return None
-    
 def greet_user(path):
     '''Greet the user by name.'''
-    username = get_stored_username(path)
-    if username:
-        print(f'Welcome back {username}.')
+    user = get_stored_name(path)
+    if user:
+        print(f'Welcome back {user['name']}. ' 
+              f'You are {user['age']} and you live in {user['city']}.')
     else:
         name_creator()
-    change_name(username)
+    change_info()
 
-def change_name(username):
+def get_stored_name(path):
+    '''Get stored name if available.'''
+    if path.exists():
+        contents = path.read_text(encoding='utf-8')
+        user = json.loads(contents)
+        return user
+    else:
+        return None
+
+def change_info():
     '''Allows the user to change the name stored in 
-    "json_files/username.json".'''
-    choice = input('Would you like to change your name? ')
-    if choice.lower() == 'yes' or choice.lower() == 'y':
+    "json_files/name.json".'''
+    choice = input('Would you like to change your info? ')
+    if (choice.lower() == 'yes' 
+    or choice.lower() == 'y' 
+    or choice.lower() == 'ya'
+    or choice.lower() == 'ye'):
         name_creator()
     else:
-        print(f'Name not changed. You are still known as {username}.')
+        user = get_stored_name(path)
+        print(f'Name not changed. You are still known as {user['name']}.')
 
 def name_creator():
-    '''Creates an initial username for the user.'''
-    username = input('What is your name? ').title()
-    contents = json.dumps(username)
+    '''Creates an initial name for the user.'''
+    name = input('What is your name? ').title()
+    while True:
+        age = input('What is your age? ')
+        try:
+            age = int(age)
+        except ValueError:
+            print('You must enter a number.')
+        else:
+            break
+    city = input('What city do you live in? ').title()
+    user = {'name'  : f'{name}',
+            'age'   : f'{age}',
+            'city'  : f'{city}',}
+    contents = json.dumps(user)
     path.write_text(contents)
-    print(f"We'll remember you when you come back, {username}!")
+    print(f"We'll remember you when you come back, {name}!")
 
 greet_user(path)
