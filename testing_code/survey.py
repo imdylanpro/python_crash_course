8feb23b324f73fc88a18d8b66bac9aaa817f78e7
Dylan Nelson <159092190+imdylanpro@users.noreply.github.com>
2024-06-02 12:59:02 -0700

    Create survey.py
    Creates an AnonymousSurvey class.

---
+++ testing_code/survey.py
@@ -0,0 +1,25 @@
+# Dylan Nelson
+# June 2, 2024
+# survey.py
+
+class AnonymousSurvey:
+    '''Collect anonymous answers to a survey question'''
+
+    def __init__(self, question):
+        '''Store a question and prepare to store responses.'''
+        self.question = question
+        self.responses = []
+
+    def show_question(self):
+        '''Display the survey question.'''
+        print(self.question)
+
+    def store_response(self, new_response):
+        '''Store the user response to the question'''
+        self.responses.append(new_response)
+
+    def show_results(self):
+        '''Show all of the user responses that have been collected.'''
+        print('Survey results:')
+        for response in self.responses:
+            print(f'- {response}')
