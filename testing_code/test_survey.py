7e189a34aba8091f0c5820bc4ac023c67b1aaed1
Dylan Nelson <159092190+imdylanpro@users.noreply.github.com>
2024-06-02 13:26:29 -0700

    Update test_survey.py
    Added a function that tests when trying to store three anonymous responses. Also included a fixture to reduce repetitive code writing for the tests.

--- testing_code/test_survey.py
+++ testing_code/test_survey.py
@@ -2,12 +2,25 @@
 # June 02, 2024
 # test_survey.py
 
+import pytest
 from survey import AnonymousSurvey
 
-def test_store_single_response():
-    '''Test that a single response to the survey is stored properly.'''
+@pytest.fixture
+def language_survey():
+    '''A survey that will be available to all test functions.'''
     question = "What language did you first learn to speak?"
     language_survey = AnonymousSurvey(question)
+    return language_survey
+
+def test_store_single_response(language_survey):
+    '''Test that a single response to the survey is stored properly.'''
     language_survey.store_response('English')
     assert 'English' in language_survey.responses
     
+def test_store_three_responses(language_survey):
+    '''Test that three individual responses are stored properly.'''
+    responses = ['Mandarin', 'English', 'Spanish']
+    for response in responses:
+        language_survey.store_response(response)
+    for response in responses:
+        assert response in language_survey.responses
