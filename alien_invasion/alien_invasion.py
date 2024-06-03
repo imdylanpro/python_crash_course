d285defe1c7b644e9504816542c66cba1de0da18
Dylan Nelson <159092190+imdylanpro@users.noreply.github.com>
2024-06-03 16:05:40 -0700

    Update alien_invasion.py
    Added a feature to control the frame rate and set the bg color to gray.

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -12,19 +12,30 @@ class AlienInvasion():
         '''Initialize the game, and create the game resources.'''
         pygame.init()
 
+        self.clock = pygame.time.clock()
+
         self.screen = pygame.display.set_mode(1200, 800)
         pygame.display.set_caption("Alien Invasion")
+
+        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         '''Start the main loop for the game.'''
         while True:
             # Watch for keyboard and mouse events
+
+            # This specifically watches for if the user clicks the exit button.
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
 
+            # Redraw the screenduring each pass through the loop
+            self.screen.fill(self.bg_color)
+
             # Make the most recently drawn screen visible.
             pygame.display.flip()
+            # Set the frame rate to 60.
+            self.clock.tick(60)
     
 if __name__ == '__main__':
     # Make a game instance, and run the game.
