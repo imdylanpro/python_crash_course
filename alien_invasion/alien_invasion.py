3fcd9cc357663833c41c747b9ad987e7baf367b0
Dylan Nelson <159092190+imdylanpro@users.noreply.github.com>
2024-06-03 17:05:50 -0700

    Update alien_invasion.py
    Refactored the run_game method. Included two helper methods to clear up the code.

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -5,38 +5,48 @@
 import sys 
 import pygame
 
+from settings import Settings
+from ship import Ship
+
 class AlienInvasion():
     '''Overall class to manage game assets and behavior.'''
 
     def __init__(self):
         '''Initialize the game, and create the game resources.'''
         pygame.init()
-
-        self.clock = pygame.time.clock()
-
-        self.screen = pygame.display.set_mode(1200, 800)
         pygame.display.set_caption("Alien Invasion")
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+        self.ship = Ship(self)
 
-        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         '''Start the main loop for the game.'''
         while True:
-            # Watch for keyboard and mouse events
-
-            # This specifically watches for if the user clicks the exit button.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
-            # Redraw the screenduring each pass through the loop
-            self.screen.fill(self.bg_color)
-
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
+            self._check_events()
+            self._update_screen()
             # Set the frame rate to 60.
             self.clock.tick(60)
     
+    # This is a helper method. It is used to only run code in the run_game 
+    # method while also being a standalone method.
+    def _check_events(self):
+        """Respond to keypreses and mouse events."""
+        # A keypress or mouse movement is considered an "event".
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                    sys.exit()
+
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+        # Redraw the screenduring each pass through the loop
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
+
 if __name__ == '__main__':
     # Make a game instance, and run the game.
     ai = AlienInvasion()
