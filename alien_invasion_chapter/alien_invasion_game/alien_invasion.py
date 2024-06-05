e0af5124ca3426eb1fa622392f0a9c59e65d8546
Dylan Nelson <159092190+imdylanpro@users.noreply.github.com>
2024-06-04 17:59:20 -0700

    Update alien_invasion.py

--- alien_invasion_chapter/alien_invasion_game/alien_invasion.py
+++ alien_invasion_chapter/alien_invasion_game/alien_invasion.py
@@ -25,6 +25,7 @@ class AlienInvasion():
         '''Start the main loop for the game.'''
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
             # Set the frame rate to 60.
             self.clock.tick(60)
@@ -37,6 +38,21 @@ class AlienInvasion():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                     sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    # Move the ship to the right while holding down keyright.
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    # Move the ship to the right while holding down keyright.
+                    self.ship.moving_left = True                    
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    # Stop movement of the ship when keyright is released.
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    # Stop movement of the ship when keyright is released.
+                    self.ship.moving_left = False
+
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
