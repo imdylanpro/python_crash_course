2265e4763bd38535ddd84e6cd3e31db8f0882960
dylan <dylannelson58@gmail.com>
2024-06-19 11:32:38 -0700

    Added functionality so that the player has 3 ships. When an alien-ship collision happend they lose one ship. A pause occurs and resets the fleet.

---
+++ alien_invasion_chapter/alien_invasion_game/game_stats.py
@@ -0,0 +1,15 @@
+# Dylan Nelson
+# June 19, 2024
+# game_stats.py
+
+class GameStats:
+    """Track statistics for Alien Invasion."""
+
+    def __init__(self, ai_game):
+        """Initialize statistics."""
+        self.settings = ai_game.settings
+        self.reset_stats()
+
+    def reset_stats(self):
+        """Initialize statistics that can change during the game."""
+        self.ships_left = self.settings.ship_limit
