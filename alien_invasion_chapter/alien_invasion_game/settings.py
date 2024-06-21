138d9aaefa0cb8c57d2affc862babf2bd00015b8
Dylan Nelson <imdylanpro@gmail.com>
2024-06-21 15:13:23 -0700

    Changing settings to be more in line with how the game is intended to be played.

--- alien_invasion_chapter/alien_invasion_game/settings.py
+++ alien_invasion_chapter/alien_invasion_game/settings.py
@@ -9,7 +9,7 @@ class Settings:
         """Initialize the game's settings."""
         # Screen settings
         self.screen_width = 1200
-        self.screen_height = 600
+        self.screen_height = 800
         self.bg_color = (230, 230, 230)
         
         # Ship settings
@@ -18,13 +18,13 @@ class Settings:
 
         # Bullet settings
         self.bullet_speed = 5.0
-        self.bullet_width = 300
+        self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 20
 
         # Alien settings
-        self.alien_speed = 10.0
+        self.alien_speed = 5.0
         self.fleet_drop_speed = 10
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
