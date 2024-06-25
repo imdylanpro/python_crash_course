31be13bc520f7ba2796894a4af0312e7cb7aa6ff
Dylan <dylannelson58@gmail.com>
2024-06-25 16:40:01 -0700

    test

--- alien_invasion_chapter/alien_invasion_game/settings.py
+++ alien_invasion_chapter/alien_invasion_game/settings.py
@@ -6,7 +6,7 @@ class Settings:
     """A class to store all of the settings for Alien Invasion."""
 
     def __init__(self):
-        """Initialize the game's settings."""
+        """Initialize the game's static settings."""
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
@@ -28,3 +28,18 @@ class Settings:
         self.fleet_drop_speed = 10
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
+
+        # How quickly the game speeds up
+        self.speedup_scale = 1.1
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Initialize settings that change throughout the game."""
+        self.ship_speed = 1.5
+        self.bullet_speed = 2.5
+        self.alien_speed = 1.0
+    
+        # Fleet direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
+        
