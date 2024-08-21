1c02b4e41bceec2e8361fe944769deca1ee9cb2e
Dylan <dylannelson58@gmail.com>
2024-08-21 15:55:19 -0700

    Removed the labels on the axes.

--- data_visualization_chapter/rw_visual.py
+++ data_visualization_chapter/rw_visual.py
@@ -9,20 +9,26 @@ from random_walk import RandomWalk
 # Keep making new walks as long as the user wants to make more.
 while True:
     # Make a random walk.
-    rw = RandomWalk()
+    rw = RandomWalk(50_000)
     rw.fill_walk()
 
     # Plot the points in the walk.
     plt.style.use('classic')
-    fig, ax = plt.subplots()
+    # build the figure and axes of the graph.
+    fig, ax = plt.subplots(figsize=(15,8), dpi = 100)
     point_numbers = range(rw.num_points)
-    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Greens, s=15, edgecolors='none')
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Greens, 
+               s=1, edgecolors='none')
     ax.set_aspect('equal')
 
     # Highlight the start and the finish of the graph.
     ax.scatter(0,0, c='blue', edgecolors='none', s=100)
     ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', 
                edgecolors='None', s=100)
+
+    # Remove the axes.
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
 
     plt.show()
 
