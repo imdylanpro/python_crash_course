1c694b794e313849a2c752288cc51e0b22c3914b
Dylan <dylannelson58@gmail.com>
2024-08-21 15:25:43 -0700

    Added functionality to emphasize first and last points.

--- data_visualization_chapter/rw_visual.py
+++ data_visualization_chapter/rw_visual.py
@@ -15,8 +15,15 @@ while True:
     # Plot the points in the walk.
     plt.style.use('classic')
     fig, ax = plt.subplots()
-    ax.scatter(rw.x_values, rw.y_values, c=rw.y_values, cmap=plt.cm.Reds, s=15)
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Greens, s=15, edgecolors='none')
     ax.set_aspect('equal')
+
+    # Highlight the start and the finish of the graph.
+    ax.scatter(0,0, c='blue', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', 
+               edgecolors='None', s=100)
+
     plt.show()
 
     keep_running = input("Generate another walk? (y/n): ")
