5af7bfd92bb326d41b68a8fac551e420bf9ab8b2
Dylan <dylannelson58@gmail.com>
2024-08-21 10:41:37 -0700

    Added in the ability for the user to continuously generate random walks if they want to.

--- data_visualization_chapter/rw_visual.py
+++ data_visualization_chapter/rw_visual.py
@@ -6,13 +6,19 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-# Make a random walk.
-rw = RandomWalk()
-rw.fill_walk()
+# Keep making new walks as long as the user wants to make more.
+while True:
+    # Make a random walk.
+    rw = RandomWalk()
+    rw.fill_walk()
 
-# Plot the points in the walk.
-plt.style.use('classic')
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, c=rw.y_values, cmap= s=15)
-ax.set_aspect('equal')
-plt.show()
+    # Plot the points in the walk.
+    plt.style.use('classic')
+    fig, ax = plt.subplots()
+    ax.scatter(rw.x_values, rw.y_values, c=rw.y_values, cmap=plt.cm.Reds, s=15)
+    ax.set_aspect('equal')
+    plt.show()
+
+    keep_running = input("Generate another walk? (y/n): ")
+    if keep_running == 'n':
+        break
