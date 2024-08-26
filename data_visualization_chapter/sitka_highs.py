cc93215eb5d57da9e286d576ccccd055821cd846
Dylan <dylannelson58@gmail.com>
2024-08-26 12:54:24 -0700

    Added the lows to the graph, also added functionality to show the fill between the highs and the lows.

--- data_visualization_chapter/sitka_highs.py
+++ data_visualization_chapter/sitka_highs.py
@@ -4,34 +4,40 @@
 
 from pathlib import Path
 import csv
+from datetime import datetime
 
 import matplotlib.pyplot as plt
 
-path = Path('weather_data/sitka_weather_07-2021_simple.csv')
+path = Path('weather_data/sitka_weather_2021_simple.csv')
 lines = path.read_text(encoding='utf-8').splitlines()
 
 reader = csv.reader(lines)
 header_row = next(reader)
+print(header_row)
 
-# Print the index of each header and the string contents.
-# for index, column_header in enumerate(header_row):
-#     print(index, column_header)
-
-# Extract the high temperatures
-highs = []
+# Extract dates and high temperatures.
+dates, highs, lows = [], [], []
 for row in reader:
+    current_date = datetime.strptime(row[2], r'%Y-%m-%d')
     high = int(row[4])
+    low = int(row[5])
+    dates.append(current_date)
     highs.append(high)
+    lows.append(low)
 # print(highs)
 
 # Plot the high temperatures
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
-ax.plot(highs, color='red')
+ax.plot(dates, highs, color='red')
+ax.plot(dates, lows, color = 'blue')
+# Fill in the difference between the highs and the lows.
+ax.fill_between(dates, highs, lows, facecolor='green', alpha=0.2)
 
 # Format plot
-ax.set_title("Daily High Temperatures, July 2021", fontsize=24)
+ax.set_title("Daily High and Low Temperatures, 2021", fontsize=24)
 ax.set_xlabel('', fontsize=16)
+fig.autofmt_xdate()
 ax.set_ylabel('Temperature (F)', fontsize=16)
 ax.tick_params(labelsize=16)
 
