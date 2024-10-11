b67d804001b0d479111200a3b2efd09318d91dfa
Dylan <dylannelson58@gmail.com>
2024-10-11 14:42:26 -0700

    Changed data to 30 days of eathquakes.

--- data_visualization_chapter/eq_explore_data.py
+++ data_visualization_chapter/eq_explore_data.py
@@ -4,16 +4,34 @@
 
 from pathlib import Path
 import json
+import plotly.express as px
 
-print('test')
 # Read data as a string and convert to a Python object.
-path = Path('eq_data/eq_data_1_day_m1.geojson')
+path = Path('eq_data/eq_data_30_day_m1.geojson')
 contents = path.read_text(encoding='utf-8')
 all_eq_data = json.loads(contents)
 
 # Examine all earthquakes in the dataset.
 all_eq_dicts = all_eq_data['features']
 print(len(all_eq_dicts))
+
+mags, lons, lats = [], [], []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
+
+title = 'Global Earthquakes'
+fig = px.scatter_geo(lat=lats, lon=lons, size=mags, title=title)
+fig.show()
+
+# Produces slices of the lists showing data that confirms we got the right info
+print(mags[:10])
+print(lons[:5])
+print(lats[:5])
 
 # Create a more readable version of the data file.
 path = Path('eq_data/readable_eq_data.geojson')
