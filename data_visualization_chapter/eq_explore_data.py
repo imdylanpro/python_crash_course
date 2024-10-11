51a50ac0fc2a8a1984e2c0ca015d7fea3a14f274
Dylan <dylannelson58@gmail.com>
2024-10-11 13:48:22 -0700

    test.

--- data_visualization_chapter/eq_explore_data.py
+++ data_visualization_chapter/eq_explore_data.py
@@ -5,10 +5,15 @@
 from pathlib import Path
 import json
 
+print('test')
 # Read data as a string and convert to a Python object.
 path = Path('eq_data/eq_data_1_day_m1.geojson')
 contents = path.read_text(encoding='utf-8')
 all_eq_data = json.loads(contents)
+
+# Examine all earthquakes in the dataset.
+all_eq_dicts = all_eq_data['features']
+print(len(all_eq_dicts))
 
 # Create a more readable version of the data file.
 path = Path('eq_data/readable_eq_data.geojson')
